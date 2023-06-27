9f1e6cc1f155bde24af728c20e6ae02c16b19b16
minzion0 <aert12435@gmail.com>
2023-06-27 10:44:26 +0900

    management select 구성

--- src/main/java/com/green/food_roulette/management/ManagementController.java
+++ src/main/java/com/green/food_roulette/management/ManagementController.java
@@ -3,20 +3,34 @@ package com.green.food_roulette.management;
 import com.green.food_roulette.management.model.ManagementMonthDto;
 import com.green.food_roulette.management.model.ManagementMonthVo;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/roulette")
+import java.util.List;
+
+@RequestMapping
 @RestController
 @Tag(name = "월별 목표")
 @RequiredArgsConstructor
 public class ManagementController {
     private final ManagementService service;
 
-    @GetMapping
-    public ManagementMonthVo getUserThisMonthManagement(){
-        return null;
+    @GetMapping("/{iuser}/roulette")
+    public ManagementMonthVo getUserThisMonthManagement(@PathVariable Long iuser){
+        ManagementMonthDto dto = new ManagementMonthDto();
+        dto.setIuser(iuser);
+        return service.getUserThisMonthManagement(dto);
     }
+
+    @GetMapping("/{iuser}/calculate")
+    public List<ManagementMonthVo> getUserManagementList(@PathVariable Long iuser){
+        ManagementMonthDto dto = new ManagementMonthDto();
+        dto.setIuser(iuser);
+        return service.getUserManagementList(dto);
+    }
+
 }
